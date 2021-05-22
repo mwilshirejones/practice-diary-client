205b25dd9e2cfbbe29abaa22a0a83a217d454290
Matthew Jones <m.wilshirejones@gmail.com>
2021-05-22 13:52:21 +0100

    MirageJS
    - Install MirageJS
    - Setup mock route and response for '/objectives'

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
 import App from './App.svelte';
+import { createServer } from 'miragejs'
+
+createServer({
+  routes() {
+    this.namespace = 'api'
+
+    this.get(
+      '/objectives',
+      () => {
+        return {
+          objectives: [
+            { id: 1, name: 'Objective 1' },
+            { id: 2, name: 'Objective 2' },
+          ]
+        }
+      },
+      // Slower server response
+      { timing: 4000 }
+    )
+  }
+})
 
 const app = new App({
 	target: document.body,
-	props: {
-		name: 'world'
-	}
 });
 
 export default app;
