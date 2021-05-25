ebb121685e2c1cce87eaea35cf4941d89c9859b4
Matthew Jones <m.wilshirejones@gmail.com>
2021-05-25 22:51:14 +0100

    Fix: router link

--- src/components/Router/Link.svelte
+++ src/components/Router/Link.svelte
@@ -1,16 +1,24 @@
 <script>
+  import { onMount } from 'svelte'
   import { location } from './store.js'
 
   export let href = ''
-
   let pathname = ''
 
   const handleClick = () => {
     location.update(loc => loc = pathname)
-    window.history.pushState({}, null, href)
+    window.history.pushState({}, null, pathname)
   }
 
-  $: pathname = `${$location}${href}`
+  onMount(() => {
+    if ($location === '/') {
+      pathname = `${$location}${href}`
+      return
+    }
+
+    let appendToDomainRoot = href.indexOf('/') === 0
+    pathname = appendToDomainRoot ? href : `${$location}/${href}`
+  })
 </script>
 
 <a href={pathname} on:click|preventDefault={handleClick}><slot></slot></a>
