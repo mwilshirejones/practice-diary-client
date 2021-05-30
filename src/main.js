9ccc79df802e8375340854fc274de0e54a62b4a2
Matthew Jones <m.wilshirejones@gmail.com>
2021-05-30 09:54:34 +0100

    Mock and fetch practice sessions

--- src/main.js
+++ src/main.js
@@ -9,12 +9,38 @@ createServer({
       '/objectives',
       () => {
         return [
-          { id: 1, name: 'Objective 1', practice_sessions: [] },
+          { 
+            id: 1, 
+            name: 'Objective 1', 
+            practice_sessions: [
+              { id: 1, name: 'Practice session 1' },
+              { id: 2, name: 'Practice session 2' },
+            ],
+          },
           { id: 2, name: 'Objective 2', practice_sessions: [] },
         ]
       },
       // Slower server response
-      { timing: 1000 }
+      { timing: 1000 },
+    )
+
+    this.get(
+      '/objectives/1/practice_sessions',
+      () => {
+        return [
+          { id: 1, name: 'Practice session 1', issues: [] },
+          { id: 2, name: 'Practice session 2', issues: [] },
+        ]
+      },
+      { timing: 1000 },
+    )
+
+    this.get(
+      '/objectives/2/practice_sessions',
+      () => {
+        return []
+      },
+      { timing: 1000 },
     )
   }
 })
--- src/views/Diary/components/Objective/Objective.svelte
+++ src/views/Diary/components/Objective/Objective.svelte
@@ -2,6 +2,9 @@
   import { Link } from '../../../../components/Router'
   import { getUrlParams } from '../../../../components/Router/helpers.js'
 
+  let practiceSessions = []
+  let loadingPracticeSessions = true
+
   export let location = ''
   let id = null
 
@@ -13,12 +16,39 @@
     if (lastParam !== 'objective') id = lastParam
   }
 
-  $: setId(location)
+  // TODO: Use async/await svelte templating
+  async function fetchPracticeSessions(objectiveId) {
+    const response = await fetch(`/api/objectives/${objectiveId}/practice_sessions`)
+
+    if (!response.ok) throw new Error(response.status)
+
+    practiceSessions = await response.json()
+    loadingPracticeSessions = false
+  }
+
+  $: {
+    setId(location)
+    practiceSessions = []
+    loadingPracticeSessions = true
+  }
+  $: fetchPracticeSessions(id)
 </script>
 
 <article>
   {#if id}
     ...fetching objective {id}
+
+    {#if loadingPracticeSessions}
+      <p>Loading practice sessions...</p>
+    {:else}
+      <ol>
+        {#each practiceSessions as practiceSession (practiceSession.id)}
+          <li><Link href={`/diary/practice-session/${practiceSession.id}`}>{practiceSession.name}</Link></li>
+        {/each}
+      </ol>
+    {/if}
+
+    <Link href="/diary/practice-session">Create a new practice session</Link>
   {:else}
     Create a new objective!
   {/if}
