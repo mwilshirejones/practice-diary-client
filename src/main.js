3f50f00f2809305734277b96a9e6685511453e77
Matthew Jones <m.wilshirejones@gmail.com>
2021-06-04 12:14:28 +0100

    Clean up miragejs mocks
    - Created `objectives` and `practiceSessions` variables
    - Added `patch` for fist objective

--- src/main.js
+++ src/main.js
@@ -3,23 +3,28 @@ import { createServer } from 'miragejs'
 
 createServer({
   routes() {
+    const objectives = [
+      { 
+        id: 1, 
+        name: 'Objective 1', 
+        practice_sessions: [
+          { id: 1, name: 'Practice session 1' },
+          { id: 2, name: 'Practice session 2' },
+        ],
+      },
+      { id: 2, name: 'Objective 2', practice_sessions: [] },
+    ]
+
+    const practiceSessions = [
+      { id: 1, name: 'Practice session 1', issues: [], objective_id: 1 },
+      { id: 2, name: 'Practice session 2', issues: [], objective_id: 1 },
+    ]
+
     this.namespace = 'api'
 
     this.get(
       '/objectives',
-      () => {
-        return [
-          { 
-            id: 1, 
-            name: 'Objective 1', 
-            practice_sessions: [
-              { id: 1, name: 'Practice session 1' },
-              { id: 2, name: 'Practice session 2' },
-            ],
-          },
-          { id: 2, name: 'Objective 2', practice_sessions: [] },
-        ]
-      },
+      () => objectives,
       // Slower server response
       { timing: 1000 },
     )
@@ -33,42 +38,28 @@ createServer({
 
     this.get(
       '/objectives/1',
-      () => {
-        return { 
-          id: 1, 
-          name: 'Objective 1', 
-          practice_sessions: [
-            { id: 1, name: 'Practice session 1' },
-            { id: 2, name: 'Practice session 2' },
-          ],
-        }
-      },
+      () => objectives[0],
       { timing: 1000 },
     )
 
     this.patch(
       '/objectives/1',
       (_, request) => {
-        console.log({ request })
+        const data = JSON.parse(request.requestBody)
+
+        return objectives[0] = { ...objectives[0], ...data }
       },
     )
 
     this.get(
       '/objectives/2',
-      () => {
-        return { id: 2, name: 'Objective 2', practice_sessions: [] }
-      },
+      () => objectives[1],
       { timing: 1000 },
     )
 
     this.get(
       '/objectives/1/practice_sessions',
-      () => {
-        return [
-          { id: 1, name: 'Practice session 1', issues: [] },
-          { id: 2, name: 'Practice session 2', issues: [] },
-        ]
-      },
+      () => practiceSessions.filter(practiceSession => practiceSession.objective_id === 1),
       { timing: 1000 },
     )
 
